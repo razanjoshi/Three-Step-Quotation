2e2182f46cad84d5a49e0037e343217e4c4d7881
Rajan Joshi <razan.joc@gmail.com>
2019-02-19 08:07:25 +0000

    Remove pry.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -85,7 +85,6 @@ class QuotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def quote_params
-      binding.pry
       params.permit(:back_button, :user_details, :user_name, :from_date, :to_date,
         :q_key, :user_password, :user_email, :user_phone, :cost,
         subscription_attributes: [:name, :daily_rate, :from_date, :to_date])
